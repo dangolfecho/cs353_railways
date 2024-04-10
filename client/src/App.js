0981f134935318b559b99d0dcaf21f04c3bb9367
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-04-10 20:21:54 +0530

    Updated component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from "./components/Login.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Home/>}/>
         </Routes>
       </BrowserRouter>
     </div>
