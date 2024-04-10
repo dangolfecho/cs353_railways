2ee8e3d41c90fd78055033a1c12ddb9f7060ffa6
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-04-10 20:17:37 +0530

    Checked whether client is able to get from server - Yes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const fetchAllUsers = async () => {
+            try{
+                const res = await axios.get("http://localhost:8000/trains");
+                setUsers(res.data);
+                console.log(res.data);
+            }catch(err){
+                console.log(err);
+            }
+        };
+        fetchAllUsers();
+    }, [])
     return (
         <div>
         <h1>Login page!</h1>
+        {users.map(user=>(
+            <div>
+            <h2>{user.username}</h2>
+            </div>
+        ))}
         </div>
     )
 };
