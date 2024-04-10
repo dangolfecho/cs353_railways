15bcf4f3b3b3eb0769f1cbbc18ba3618c096356b
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-04-10 20:30:42 +0530

    Wrote login functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,28 +2,27 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Login = () => {
-    const [users, setUsers] = useState([])
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
-    useEffect(() => {
-        const fetchAllUsers = async () => {
-            try{
-                const res = await axios.get("http://localhost:8000/trains");
-                setUsers(res.data);
-                console.log(res.data);
-            }catch(err){
-                console.log(err);
-            }
-        };
-        fetchAllUsers();
-    }, [])
+    const handleUserChange = (event) => {
+        setUsername(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
     return (
         <div>
-        <h1>Login page!</h1>
-        {users.map(user=>(
-            <div>
-            <h2>{user.username}</h2>
-            </div>
-        ))}
+        <h1>Login page</h1>
+        <label for="username">Enter your username</label>
+        <input id="username" name="username" type='text' onChange={handleUserChange}></input>
+        <br/>
+        <label for="password">Enter your password</label>
+        <input id="password" name="password" type='password' onChange={handlePasswordChange}></input>
+        <br/>
+        <button type="submit">Login</button>
         </div>
     )
 };
