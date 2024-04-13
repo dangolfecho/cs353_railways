25aa83755761e7d4c60fbee20d29cebea07cc8a5
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2024-04-14 00:31:31 +0530

    Changed schema

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.js";
 import Register from "./components/Register.js";
-import PNR from "./components/PNR.js";
+import Pnr from "./components/Pnr.js";
 import Main from "./components/Main.js";
 import Search from "./components/Search.js";
 import Reservation from "./components/Reservation.js";
@@ -15,7 +15,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/register' element={<Register />} />
-                    <Route path='/pnr' element={<PNR />} />
+                    <Route path='/pnr' element={<Pnr />} />
                     <Route path='/home' element={<Main />} />
                     <Route path='/tickets' element={<Tickets />} />
                     <Route path='/search' element={<Search />} />
